0b15a991ab8f9e93f28715acf937c8c407dc1fcc
David Gajdos <david.gajdos97@gmail.com>
2023-12-06 19:27:00 +0100

    Sixth day done

---
+++ 06/6_1.cpp
@@ -0,0 +1,68 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+std::vector<int> get_numbers(const std::string& line) {
+    std::vector<int> out;
+
+    auto colon_pos = line.find(':');
+    colon_pos++;
+
+    while (colon_pos < line.size()) {
+        while (colon_pos < line.size() && std::isspace(line[colon_pos])) colon_pos++;
+
+        int num = 0;
+        while (colon_pos != line.size() && std::isdigit(line[colon_pos])) {
+            num = num * 10 + (line[colon_pos] - '0');
+            colon_pos++;
+        }
+
+        out.push_back(num);
+    }
+
+    return out;
+}
+
+int get_winning_ways_count(int time, int distance) {
+    int start = static_cast<int>(time/2) + 1;
+    int count = 0;
+
+    auto f = [time](int x) -> int {
+        return -(x*x) + time*x;
+    };
+
+    while (f(start) > distance) {
+        count++;
+        start++;
+    }
+    // symmetric
+    count = count*2;
+
+    return time % 2 == 0 ? count + 1 : count;
+}
+
+int main() {
+    std::string time_line;
+    std::string distance_line;
+
+    std::getline(std::cin, time_line);
+    std::getline(std::cin, distance_line);
+
+    auto times = get_numbers(time_line);
+    auto distances = get_numbers(distance_line);
+
+    assert(times.size() == distances.size());
+
+    int product = 1;
+    for (std::size_t i = 0; i < distances.size(); i++) {
+        int time = times[i];
+        int distance = distances[i];
+
+        int count = get_winning_ways_count(time, distance);
+
+        product *= count;
+    }
+
+    std::cout << product << '\n';
+}
